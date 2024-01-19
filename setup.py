ad34bdba57d2aa1bf2ad32822e20c84f1c8db217
Rajesh P <rajesh.pethe@gmail.com>
2024-01-19 16:26:58 +0530

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bhashini_translator",
-    version="0.1",
+    version="0.2",
     packages=["bhashini_translator"],
     install_requires=["requests"],
     author="Rajesh Pethe",
